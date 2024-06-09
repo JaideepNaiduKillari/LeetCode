6657beae01162a196720fa4be5dc928497636cff
JaideepNaiduKillari <143888866+JaideepNaiduKillari@users.noreply.github.com>
2024-06-09 15:42:37 +0530

    Time: 35 ms (99.07%) | Memory: 16.5 MB (91.62%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -3,9 +3,7 @@ class Solution:
         x=str(x)
         y=len(x)
         for i in range(0,y//2):
-            if x[i] == x[y-i-1]:
-                continue
-            else:
-                return False
+            if x[i] != x[y-i-1]:
+                return False  
         return True
 
