6a4db091573777e784c8912236181d5f68305cc4
Killari Jaideep Naidu <143888866+JaideepNaiduKillari@users.noreply.github.com>
2025-03-24 15:42:02 +0530

    Time: 3 ms (41.99%) | Memory: 41.9 MB (47.33%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,41 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Integer> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
         for(int i = 0;i<s.length();++i){
-            if(s.charAt(i) == '('){
-                stack.push(1);
-            }
-            if(s.charAt(i) == ')'){
-                if(stack.isEmpty()){return false;}
-                if(stack.peek() == 1){stack.pop();}
-                else{
-                    stack.push(-1);
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+                stack.push(s.charAt(i));
+            }else{
+                if(stack.isEmpty()){
+                    return false;
                 }
-            }
-            if(s.charAt(i) == '['){
-                stack.push(2);
-            }
-            if(s.charAt(i) == ']'){
-                if(stack.isEmpty()){return false;}
-                if(stack.peek() == 2){stack.pop();}
-                else{
-                    stack.push(-1);
+                char ch = stack.pop();
+                if(s.charAt(i) == ')' && ch == '('||s.charAt(i) == ']' && ch == '[' || s.charAt(i) == '}' && ch == '{'){
+                    continue;
                 }
-            }
-            if(s.charAt(i) == '{'){
-                stack.push(3);
-            }
-            if(s.charAt(i) == '}'){
-                if(stack.isEmpty()){return false;}
-                if(stack.peek() == 3){stack.pop();}
-                else{
-                    stack.push(-1);
-                }
+                else return false;
             }
         }
-        if(stack.isEmpty()){
-            return true;
-        }
-        return false;
+
+        return stack.isEmpty();
     }
 }
