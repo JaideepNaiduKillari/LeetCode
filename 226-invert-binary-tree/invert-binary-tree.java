db48fa256c549524e16502830ccf7f4c813e9965
Killari Jaideep Naidu <143888866+JaideepNaiduKillari@users.noreply.github.com>
2025-05-22 21:39:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (25.59%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -15,11 +15,10 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null)return null;
+        if(root == null) return root;
         TreeNode temp = root.left;
         root.left = invertTree(root.right);
         root.right = invertTree(temp);
-            
         return root;
     }
 }
