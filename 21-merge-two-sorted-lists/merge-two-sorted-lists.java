86102301dd5379d7928cd0c0cd80a93833d56df1
Killari Jaideep Naidu <143888866+JaideepNaiduKillari@users.noreply.github.com>
2025-05-25 00:29:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (59.61%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,47 +10,45 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode answerhead = null;
         if(list1 == null){
             return list2;
         }
         if(list2 == null){
             return list1;
         }
-
-
-        ListNode answerNode = new ListNode();
+        if(list1 == null && list2 == null)return answerhead;
+        
         if(list1.val < list2.val){
-            answerNode.val = list1.val;
+            answerhead = list1;
             list1 = list1.next;
         }else{
-            answerNode.val = list2.val;
+            answerhead = list2;
             list2 = list2.next;
         }
-
-        ListNode temp = answerNode;
+        ListNode root = answerhead;
         while(list1 != null && list2 != null){
             if(list1.val < list2.val){
-                temp.next = list1;
+                answerhead.next = list1;
                 list1 = list1.next;
-                temp = temp.next;
+                answerhead = answerhead.next;
             }
             else{
-                temp.next = list2;
+                answerhead.next = list2;
                 list2 = list2.next;
-                temp = temp.next;
+                answerhead = answerhead.next;
             }
         }
         while(list1 != null){
-            temp.next = list1;
+            answerhead.next = list1;
             list1 = list1.next;
-            temp = temp.next;
+            answerhead = answerhead.next;
         }
         while(list2 != null){
-            temp.next = list2;
+            answerhead.next = list2;
             list2 = list2.next;
-            temp = temp.next;
+            answerhead = answerhead.next;   
         }
-
-        return answerNode;
+        return root;
     }
 }
